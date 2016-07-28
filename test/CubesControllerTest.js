4491374f725d72aa5add54f243adacfbf970f0b4
muceres <muceres+github@openmailbox.org>
2016-07-28 17:07:47 +0200

    test(CubesControllersTest): add test for missing 'name' field

--- test/CubesControllerTest.js
+++ test/CubesControllerTest.js
@@ -2,22 +2,36 @@ var assert = require('chai').assert,
     expect = require('chai').expect,
     should = require('chai').should(),
     express = require('express'),
-    request = require('supertest');
+    superTest = require('supertest');
 
 // TODO: apiUrl should be configurable by an options param
 var apiUrl = 'http://localhost:8080';
-var request = request(apiUrl);
+var request = superTest(apiUrl);
 
 describe('CubesController', function() {
     
     describe('POST /cubes - Create a cube', function() {
 	describe('Cases : 400 errors and "fail" status"', function() {
 	    describe('missing_parameter:', function() {
-		it('should return a "name" missing message');
+		it('should detect a missing \'name\' field', function(done){
+		    request.post('/cubes')
+			.send({name: "lala"})
+			.set('Accept', 'application/json')
+			.expect('Content-Type', /json/)
+			.expect(400)
+			.end(function(err, res){
+			    if (err) throw err;
+			    expect(res.body.status).to.equal('fail');
+			    expect(res.body.data.error_code).to.equal('missing_parameters');
+			    expect(res.body.data.name).to.equal('a name is required');
+			    done();
+			});
+		});
 	    });
-	    describe('invalid_parameter: ', function() {
+	    describe('invalid_parameter:', function() {
 		describe('name:', function() {
-		    it('should return a size error if "name" range is not between 2 and 150');
+		    it('should return a size error if "cover"\'s size range is incorrect');
+		    it('should return a format error if "cover" format isn\'t accepted');
 		});
 		describe('cover:', function() {
 		    it('should return a size error if "cover"\'s size range is incorrect');
@@ -26,6 +40,4 @@ describe('CubesController', function() {
 	    });
 	});
     });
-
 });
-
